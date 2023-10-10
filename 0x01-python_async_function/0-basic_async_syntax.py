cc4ad44e4e2ba6148da8805574900451b70fa5a0
FrancisOkolo <francischinedu78@gmail.com>
2023-10-10 05:21:04 +0100

    pep8 compliant

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -6,7 +6,7 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay: int = 10) -> float:
+async def wait_random(max_delay: int=10) -> float:
     " return a random float in random time"
     b = random.uniform(0, max_delay)
     await asyncio.sleep(max_delay)
