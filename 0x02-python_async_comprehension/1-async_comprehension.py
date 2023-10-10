86c26c06c875b24bb41ebd5ddf4eefe189c09269
FrancisOkolo <francischinedu78@gmail.com>
2023-10-10 20:16:17 +0100

    task 2

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -4,8 +4,10 @@
 import asyncio
 from typing import Generator
 import random
-async_generator = find('0-async_generator').async_generator
 from importlib import import_module as find
+
+
+async_generator = find('0-async_generator').async_generator
 
 
 async def async_comprehension() -> Generator[float, None, None]:
