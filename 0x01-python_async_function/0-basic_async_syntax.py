e3f9aa6170565a40a6db62b86c4d884eaa65d578
FrancisOkolo <francischinedu78@gmail.com>
2023-10-10 04:11:15 +0100

    task 1

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+#basic async syntax
+
+
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    " return a random float in random time"import asyncio
+import random
+
+async def wait_random(max_delay: int = 10) -> float:
+    b = random.uniform(0, max_delay)
+    await asyncio.sleep(max_delay)
+    return b
+    b = random.uniform(0, max_delay)
+    await asyncio.sleep(max_delay)
+    return b
---
+++ 0x01-python_async_function/README.md
@@ -0,0 +1,1 @@
+Learning asynchronous programming in python
